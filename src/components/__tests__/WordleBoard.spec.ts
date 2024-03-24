d8b326c9b849bffc14cb3e38e745bb43e172439f
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-03-23 20:55:26 -0400

    Remove clutter

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,15 +3,12 @@ import WordleBoard from "../WordleBoard.vue";
 
 describe("WordleBoard", () => {
   test("A victory message appears when the user guess the word", async () => {
-    // Arrange
     const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
 
-    // Act
     const guessInput = wrapper.find("input[type=text]");
     await guessInput.setValue("TESTS");
     await guessInput.trigger("keydown.enter");
 
-    // Assert
     expect(wrapper.text()).contain("You won!");
   });
 });
